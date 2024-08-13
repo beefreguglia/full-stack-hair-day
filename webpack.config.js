179b56f494cd5caf8d5b141522eca0471e2943c3
Bernardo <beefreguglia@gmail.com>
2024-08-13 14:56:51 -0300

    feature: include html on webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   target: "web",
@@ -15,5 +16,10 @@ module.exports = {
     port: 3000,
     open: true,
     liveReload: true,
-  }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+    }),
+  ]
 }
