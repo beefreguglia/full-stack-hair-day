085c07d8f74b7e09ce572030498cbfb74a5892a7
Bernardo <beefreguglia@gmail.com>
2024-08-13 16:44:32 -0300

    feature: separe in morning afternoon and night

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,6 +1,9 @@
 
 import dayjs from "dayjs";
+
 import { openingHours } from "../../utils/opening-hours.js";
+
+const hours = document.getElementById("hours");
 
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
@@ -15,4 +18,28 @@ export function hoursLoad({ date }) {
       available: isHourAvailable,
     }
   });
+
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li");
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+    li.textContent = hour;
+
+    if(hour === "9:00") {
+      hourHeaderAdd("Manhã");
+    } else if (hour === "13:00") {
+      hourHeaderAdd("Tarde");
+    } else if (hour === "18:00") {
+      hourHeaderAdd("Noite");
+    }
+    hours.append(li);
+  })
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li");
+  header.classList.add("hour-period");
+  header.textContent = title;
+
+  hours.append(header);
 }
