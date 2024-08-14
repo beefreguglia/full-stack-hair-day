5dd2e233c90c163f084dbac9968a4bc271e179d2
Bernardo <beefreguglia@gmail.com>
2024-08-14 19:23:53 -0300

    feature: Loading schedules for other days

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -7,6 +7,8 @@ import { hoursClick } from './hour-click.js';
 const hours = document.getElementById("hours");
 
 export function hoursLoad({ date }) {
+  // Limpa a lista de horários.
+  hours.innerHTML = "";
   const opening = openingHours.map((hour) => {
     // Recupera somente a hora.
     const [scheduleHour] = hour.split(":");
