2b2e951ca3b3d4530ab24954b60894fe219892ad
Bernardo <beefreguglia@gmail.com>
2024-08-14 22:40:16 -0300

    feature: Rendering the day's schedules

--- src/modules/schedules/schedules-load.js
+++ src/modules/schedules/schedules-load.js
@@ -1,16 +1,23 @@
-import { hoursLoad } from "../form/hours-load";
+import { scheduleFetchByDay } from "../../services/schedule-fetch-by-day.js";
+import { scheduleShow } from "./schedules-show.js";
+import { hoursLoad } from "../form/hours-load.js";
 
 // Seleciona o input de data
 const selectedDateInput = document.getElementById("date");
 
-export function schedulesDays() {
+export async function schedulesDays() {
   // Obtém a data do input
   const selectedDate = selectedDateInput.value;
+
+  // Busca na API os agendamentos para carregar do lado direito na tela.
+  const dailySchedules = await scheduleFetchByDay({  date: selectedDate });
+ 
+  // Exibe os agendamentos.
+  scheduleShow({ dailySchedules });
 
   //Renderiza as horas disponíveis.
   hoursLoad({ date: selectedDate });
   
-  // Busca na API os agendamentos para carregar do lado direito na tela.
 
   // Os horários disponíveis (horário futuro + não agendado).
 
